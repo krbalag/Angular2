b2db86483650115e0a18706d1a1bedf89105b488
krbalag <krbalag@gmail.com>
2018-02-05 14:53:14 -0800

    Add files via upload

---
+++ event-binding/angular2-demo/app/module.ts
@@ -0,0 +1,10 @@
+import {NgModule}       from '@angular/core';
+import {BrowserModule}  from '@angular/platform-browser';
+import {EventComponent}  from './event.component';
+@NgModule({
+  imports:      [BrowserModule],
+  declarations: [EventComponent],
+  bootstrap:    [EventComponent]
+})
+export class AppModule { }
+  
