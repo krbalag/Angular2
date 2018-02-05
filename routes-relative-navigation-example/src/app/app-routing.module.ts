d75c663698982fc75e8911ccf4c95e75758af65a
krbalag <krbalag@gmail.com>
2018-02-05 14:48:33 -0800

    Add files via upload

---
+++ routes-relative-navigation-example/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule }      from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddressComponent }  from './address.component';
+import { PageNotFoundComponent }  from './page-not-found.component';
+
+const routes: Routes = [
+	{
+	   path: 'country',
+       loadChildren: 'app/country/country.module#CountryModule',
+       data: { preload: true }
+	},
+	{
+	   path: 'person',
+       loadChildren: 'app/person/person.module#PersonModule'
+	},
+	{
+	   path: 'address',
+	   component: AddressComponent
+	},	
+	{
+	   path: '',
+	   redirectTo: '/country',
+	   pathMatch: 'full'
+	},
+    {
+	   path: '**',
+	   component: PageNotFoundComponent 
+    }	
+];
+@NgModule({
+  imports: [ 
+          RouterModule.forRoot(routes) 
+  ],
+  exports: [ 
+          RouterModule 
+  ]
+})
+export class AppRoutingModule{ } 
